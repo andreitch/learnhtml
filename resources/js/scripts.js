9d0c1418e0a499dc55edb656453a48ac8fd92791
andreitch <andreitch.top@gmail.com>
2020-05-11 20:45:08 +0300

    responsive navigation

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -74,9 +74,25 @@ $(document).ready(function() {
   }, {
     offset: '50%'
   });
+  
   $('.js--wp-4').waypoint(function(direction) {
     $('.js--wp-4').addClass('animate__animated animate__pulse');
   }, {
     offset: '50%'
   });
+  
+  /* Mobile nav */
+  $('.js--nav-icon').click(function() {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+    
+    nav.slideToggle(200);
+    if (icon.hasClass('ion-navicon-round')) {
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round');
+    } else {
+      icon.addClass('ion-navicon-round');
+      icon.removeClass('ion-close-round');
+    }
+  })
 });
