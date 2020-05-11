99f9a9c66f2844b0254b49ca91e65caa4e71ba6d
andreitch <andreitch.top@gmail.com>
2020-05-11 20:16:31 +0300

    animations on scroll

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
             // Callback after animation
             // Must change focus!
             var $target = $(target);
-            $target.focus();
+            $target.focus(); 
             if ($target.is(":focus")) { // Checking if the target was focused
               return false;
             } else {
@@ -54,5 +54,29 @@ $(document).ready(function() {
         }
       }
     });
+  });
+  
+  /* Animationson scroll */
+  $('.js--wp-1').waypoint(function(direction) {
+    $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+  }, {
+    offset: '50%'
+  });
+  
+  $('.js--wp-2').waypoint(function(direction) {
+    $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
+  }, {
+    offset: '50%'
+  });
+  
+  $('.js--wp-3').waypoint(function(direction) {
+    $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+  }, {
+    offset: '50%'
+  });
+  $('.js--wp-4').waypoint(function(direction) {
+    $('.js--wp-4').addClass('animate__animated animate__pulse');
+  }, {
+    offset: '50%'
   });
 });
